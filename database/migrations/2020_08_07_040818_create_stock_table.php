4c48809f33bec14db3a6254dda29ef17b7505d60
pluem <pluem393@gmail.com>
2020-08-07 11:16:00 +0700

    07/08/2020

---
+++ database/migrations/2020_08_07_040818_create_stock_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStockTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stock', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+
+            $table->integer('valua')->nullable();
+            $table->float('weight')->nullable();
+            $table->string('name')->nullable();
+            $table->date('date')->nullable();
+            $table->text('description')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stock');
+    }
+}
